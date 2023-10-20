5cb4322d8a43b3c35b2a17c7b6861c74eff98ac3
KEVIN VAN SCHELLEBECK <kevin.vanschellebeck@gmail.com>
2023-10-20 14:40:50 +0200

    modification banniere_body de l'accueil

--- src/components/BodyPicture.jsx
+++ src/components/BodyPicture.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "../styles/bodyPicture.css";
-import picture from "../assets/body_picture.png";
+// import picture from "../assets/body_picture.png";
 
 const bodyPicture = () => {
   return (
     <div className="banniere">
-      <img src={picture} alt="banniere" />
+      {/* <img src={picture} alt="banniere" /> */}
       <p>Chez vous, partout et ailleurs</p>
     </div>
   );
