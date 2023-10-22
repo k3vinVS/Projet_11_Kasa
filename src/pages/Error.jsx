4b39fce44b2c2d2f30fc8b69440e2de6295717e9
KEVIN VAN SCHELLEBECK <kevin.vanschellebeck@gmail.com>
2023-10-22 17:05:10 +0200

    Mise en place de la page d'erreur, à finaliser

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import "../styles/errorPage/error.css";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>
-      <h1>ERROR 404</h1>
+    <div className="container">
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="accueil">
+        Retourner sur la page d'accueil
+      </Link>
     </div>
   );
 };
